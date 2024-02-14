b639776d3b841afc89ad240689ccb7e24df352ef
amdouyah <aminedouyahia00@gmail.com>
2024-02-14 21:52:47 +0100

    modif ex01

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -16,6 +16,14 @@ static int calcul(int a, int b, char c){
 }
 void rpn(std::string s){
 	std::stack <int> stk;
+	size_t i = 0;
+	while(i < s.length() && s[i] == ' ')
+		i++;
+	if(!s[i])
+		throw std::runtime_error("Error");
+	if (!s.length() && !isdigit(s[0]) && s[0] != '*'
+			&& s[0] != '+' && s[0] != '-' && s[0] != '/')
+		throw std::runtime_error("Error");
 	std::istringstream inp(s);
 	std::string str;
 	while(inp >> str){
